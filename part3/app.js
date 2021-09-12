16e995809516dfa20fa8a02e47e94164e3aa9ffe
jjanmo <jjanmo07@hanmail.net>
2021-09-12 23:11:13 +0900

    feat: add character and add feature of moving character

--- part3/app.js
+++ part3/app.js
@@ -4,3 +4,54 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 canvas.style.backgroundColor = '#eee';
+
+// charater
+const dino = {
+  x: 10,
+  y: 200,
+  width: 50,
+  height: 50,
+  draw() {
+    ctx.fillStyle = 'green';
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  },
+};
+dino.draw();
+
+class Cactus {
+  constructor() {
+    this.x = window.innerWidth - 50;
+    this.y = 200;
+    this.width = 50;
+    this.height = 50;
+  }
+
+  draw() {
+    ctx.fillStyle = 'red';
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+let timer = 0;
+const cactuses = [];
+
+function move() {
+  requestAnimationFrame(move);
+  timer++;
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  if (timer % 60 === 0) {
+    const cactus = new Cactus();
+    cactuses.push(cactus);
+  }
+
+  cactuses.forEach(catcus => {
+    catcus.x -= 5;
+    catcus.draw();
+  });
+
+  dino.draw();
+}
+
+move();
