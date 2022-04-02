22bd7792f96d8201bd2e2ebda85536bb49180c90
jjanmo <jjanmo07@hanmail.net>
2022-04-02 23:59:21 +0900

    feat: apply transition

---
+++ part4/bonus_transform_animation/app1.js
@@ -0,0 +1,36 @@
+const $boxs = document.querySelectorAll('.box');
+const $buttons = document.querySelector('.buttons');
+const $radios = document.querySelectorAll('.radio');
+
+let origin = 'cc';
+
+$buttons.addEventListener('click', (e) => {
+  const target = e.target;
+  const originText = target.textContent;
+  const _origin = target.className;
+
+  if (target.tagName !== 'BUTTON') return;
+
+  document.querySelector(`.${origin}`).classList.remove('clicked');
+  target.classList.add('clicked');
+
+  origin = _origin;
+  $boxs.forEach(($box) => {
+    $box.style.transformOrigin = originText;
+  });
+});
+
+$radios.forEach(($radio) => {
+  $radio.addEventListener('click', (e) => {
+    console.log(e.target, e.target.value);
+    if (e.target.value === '적용') {
+      $boxs.forEach(($box) => {
+        $box.classList.add('transition');
+      });
+    } else {
+      $boxs.forEach(($box) => {
+        $box.classList.remove('transition');
+      });
+    }
+  });
+});
