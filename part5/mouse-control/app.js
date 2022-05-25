b7c48ca2071f3012f7cf9c6806f645094c12a6f3
jjanmo <jjanmo07@hanmail.net>
2022-05-25 23:27:49 +0900

    feat: update mouse moving using css

--- part5/mouse-control/app.js
+++ part5/mouse-control/app.js
@@ -1,26 +1,30 @@
 (function () {
-  let speed = 0.09;
-
-  // 마우스가 움직이는 위치(정해지는 값)
-  let posX = 0,
-    posY = 0,
-    // 궁극적으로 마우스가 posX와 posY로 가기위해서 거쳐가는 값
-    mX = 0,
-    mY = 0;
+  // let speed = 0.09;
+  // // 마우스가 움직이는 위치(정해지는 값)
+  // let posX = 0,
+  //   posY = 0,
+  //   // 궁극적으로 마우스가 posX와 posY로 가기위해서 거쳐가는 값
+  //   mX = 0,
+  //   mY = 0;
   const $star = document.querySelector('.star');
 
-  const move = () => {
-    mX += (posX - mX) * speed;
-    mY += (posY - mY) * speed;
-    $star.style.transform = `translate3D(${mX}px, ${mY}px, 0)`;
+  // 1) 자바스크립트를 이용하는 방법
+  // const move = () => {
+  //   mX += (posX - mX) * speed;
+  //   mY += (posY - mY) * speed;
+  //   $star.style.transform = `translate3D(${mX}px, ${mY}px, 0)`;
 
-    window.requestAnimationFrame(move);
-  };
+  //   window.requestAnimationFrame(move);
+  // };
+  // move();
 
-  window.addEventListener('mousemove', (e) => {
-    posX = e.clientX;
-    posY = e.clientY;
+  // window.addEventListener('mousemove', (e) => {
+  //   posX = e.clientX;
+  //   posY = e.clientY;
+  // });
+
+  // 2) CSS를 이용하는 움직임
+  window.addEventListener('click', (e) => {
+    $star.style.transform = `translate3D(${e.clientX}px, ${e.clientY}px, 0)`;
   });
-
-  move();
 })();
