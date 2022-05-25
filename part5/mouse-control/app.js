f663f37dc728068da4b94773d49c6998ffe46487
jjanmo <jjanmo07@hanmail.net>
2022-05-25 23:20:19 +0900

    feat: update mouse moving

--- part5/mouse-control/app.js
+++ part5/mouse-control/app.js
@@ -1,10 +1,26 @@
 (function () {
+  let speed = 0.09;
+
+  // 마우스가 움직이는 위치(정해지는 값)
+  let posX = 0,
+    posY = 0,
+    // 궁극적으로 마우스가 posX와 posY로 가기위해서 거쳐가는 값
+    mX = 0,
+    mY = 0;
   const $star = document.querySelector('.star');
-  console.log($star);
+
+  const move = () => {
+    mX += (posX - mX) * speed;
+    mY += (posY - mY) * speed;
+    $star.style.transform = `translate3D(${mX}px, ${mY}px, 0)`;
+
+    window.requestAnimationFrame(move);
+  };
+
   window.addEventListener('mousemove', (e) => {
-    console.log(e.clientX, e.clientY);
-    const posX = e.clientX;
-    const posY = e.clientY;
-    $star.style.transform = `translate3D(${posX}px, ${posY}px, 0)`;
+    posX = e.clientX;
+    posY = e.clientY;
   });
+
+  move();
 })();
