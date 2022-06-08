c20d0bd9a60dfdab2102d0d444db7fe4734acc87
jjanmo <jjanmo07@hanmail.net>
2022-06-09 00:19:38 +0900

    feat: update image position for scroll

--- part5/parallax2/app.js
+++ part5/parallax2/app.js
@@ -4,3 +4,8 @@ const $image3 = document.querySelector('.image3');
 const $image4 = document.querySelector('.image4');
 const $image5 = document.querySelector('.image5');
 const $image6 = document.querySelector('.image6');
+
+window.addEventListener('scroll', () => {
+  const scrolled = document.documentElement.scrollHeight;
+  console.log(scrolled);
+});
