5a02000aa62dea67a2a02cc3860adbf44c93f95b
jjanmo <jjanmo07@hanmail.net>
2022-06-24 11:10:13 +0900

    feat: add various buttons logic(image button, erase button)

--- part6/10-painting.js
+++ part6/10-painting.js
@@ -1,19 +1,40 @@
 const canvas = document.querySelector('.canvas');
 const context = canvas.getContext('2d');
 
-const $palette = document.querySelector('.palette');
+const $controls = document.querySelector('.controls');
 const $brush = document.querySelector('#brush');
 const $brushValue = document.querySelector('.brush-value');
 
-let currentColor = '#f1c40f';
+let previousColorButton = document.querySelector('.black');
 let brushWidth = 10;
+let isPainting = false;
+let currentMode = 'none'; // none | paint | erase | image
+let currentColor = 'black';
+let currentImage = '';
 
-$palette.addEventListener('click', (e) => {
+$controls.addEventListener('click', (e) => {
   const target = e.target;
 
   if (target.tagName === 'BUTTON') {
-    const color = target.id;
-    currentColor = color;
+    // 버튼 로직
+    const mode = target.dataset.mode;
+
+    if (mode === 'paint') {
+      const color = target.id;
+      currentColor = color;
+    } else if (mode === 'erase') {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      return;
+    } else if (mode === 'image') {
+      const src = `./assets/${target.id}.png`;
+      currentImage = src;
+    }
+    currentMode = mode;
+
+    // 버튼 스타일 변경
+    previousColorButton.classList.remove('active');
+    previousColorButton = target;
+    target.classList.add('active');
   }
 });
 
@@ -23,11 +44,26 @@ $brush.addEventListener('input', (e) => {
   brushWidth = value;
 });
 
-canvas.addEventListener('click', (e) => {
+canvas.addEventListener('mouseup', () => {
+  isPainting = false;
+});
+canvas.addEventListener('mousedown', () => {
+  isPainting = true;
+});
+
+canvas.addEventListener('mousemove', (e) => {
+  if (!isPainting) return;
+
   const posX = e.offsetX;
   const posY = e.offsetY;
-
-  context.beginPath();
-  context.arc(posX, posY, 10, 0, Math.PI * 2, false);
-  context.fill();
+  if (currentMode === 'paint') {
+    context.beginPath();
+    context.arc(posX, posY, brushWidth / 2, 0, Math.PI * 2, false);
+    context.fillStyle = currentColor;
+    context.fill();
+  } else if (currentMode === 'image') {
+    const image = new Image();
+    image.src = currentImage;
+    context.drawImage(image, posX, posY, 40, 40);
+  }
 });
