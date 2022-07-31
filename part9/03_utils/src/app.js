ef2599afef54e3bc97b7ec942c915ecedb68661f
jjanmo <jjanmo07@hanmail.net>
2022-07-31 13:48:00 +0900

    feat: initial 03chapter

---
+++ part9/03_utils/src/app.js
@@ -0,0 +1,4 @@
+import './style.css'
+import example from './examples/01_helper'
+
+example()
---
+++ part9/03_utils/src/examples/01_helper.js
@@ -0,0 +1,52 @@
+import * as THREE from 'three'
+
+export default function example() {
+  const canvas = document.getElementById('my-canvas')
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  })
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1)
+
+  const scene = new THREE.Scene()
+  scene.background = new THREE.Color('#ecf0f1')
+
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
+  camera.position.x = 1
+  camera.position.y = 2
+  camera.position.z = 5
+  camera.lookAt(0, 0, 0)
+  scene.add(camera)
+
+  const light = new THREE.DirectionalLight(' 0xffffff', 1)
+  light.position.y = 2
+  light.position.z = 5
+  scene.add(light)
+
+  const geometry = new THREE.BoxGeometry(1, 1, 1)
+  const matarial = new THREE.MeshStandardMaterial({ color: '#3498db' })
+  const cube = new THREE.Mesh(geometry, matarial)
+  scene.add(cube)
+
+  renderer.render(scene, camera)
+
+  const clock = new THREE.Clock()
+  const draw = () => {
+    const time = clock.getDelta()
+    cube.rotation.y += THREE.MathUtils.degToRad(time * 100)
+
+    renderer.render(scene, camera)
+    requestAnimationFrame(draw)
+  }
+  draw()
+
+  const handleResizeCanvas = () => {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    renderer.render(scene, camera)
+  }
+
+  window.addEventListener('resize', handleResizeCanvas)
+}
