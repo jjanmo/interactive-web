2403564e748c2765424426dbc12c999a7e8b3b35
jjanmo <jjanmo07@hanmail.net>
2022-07-31 19:59:42 +0900

    feat: how to makeup grouping meshes

--- part9/04_transfrom/src/app.js
+++ part9/04_transfrom/src/app.js
@@ -1,4 +1,5 @@
 import './style.css'
-import example from './exmaples/01_position'
+// import example from './exmaples/01_position_scale_rotate'
+import example from './exmaples/02_scen-graph'
 
 example()
--- part9/04_transfrom/src/exmaples/02_scen-graph.js
+++ part9/04_transfrom/src/exmaples/02_scen-graph.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three'
 import * as dat from 'dat.gui'
 
+// scene-graph : 그룹만들기
+
 export default function example() {
   const canvas = document.getElementById('my-canvas')
   const renderer = new THREE.WebGLRenderer({
@@ -17,10 +19,9 @@ export default function example() {
   scene.add(axesHelper)
 
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
-  camera.position.x = 1
-  camera.position.y = 2
+  camera.position.x = 0
+  camera.position.y = 1
   camera.position.z = 5
-  camera.lookAt(0, 0, 0)
   scene.add(camera)
 
   const light = new THREE.DirectionalLight('#eee', 1)
@@ -30,8 +31,27 @@ export default function example() {
 
   const geometry = new THREE.BoxGeometry(1, 1, 1)
   const matarial = new THREE.MeshStandardMaterial({ color: '#3498db' })
-  const cube = new THREE.Mesh(geometry, matarial)
-  scene.add(cube)
+
+  // Like Solar System
+  const group1 = new THREE.Group()
+  const cube1 = new THREE.Mesh(geometry, matarial) // 태양
+
+  const group2 = new THREE.Group()
+  // const cube2 = new THREE.Mesh(geometry, matarial) // 1) 1 & 2는 같은 결과물
+  const cube2 = cube1.clone() // 2) // 지구
+  cube2.scale.set(0.3, 0.3, 0.3)
+  group2.position.x = 3
+
+  // const group3 = new THREE.Group()
+  const group3 = new THREE.Object3D() // Group()대신 사용가능 // 달
+  const cube3 = cube2.clone()
+  cube3.scale.set(0.1, 0.1, 0.1)
+  group3.position.x = 0.7
+
+  group3.add(cube3)
+  group2.add(cube2, group3)
+  group1.add(cube1, group2)
+  scene.add(group1)
 
   renderer.render(scene, camera)
 
@@ -43,9 +63,9 @@ export default function example() {
   const clock = new THREE.Clock()
   const draw = () => {
     const delta = clock.getDelta()
-    cube.rotation.y += THREE.MathUtils.degToRad(delta * 100)
-
-    camera.lookAt(cube.position)
+    group1.rotation.y += delta * 0.8
+    group2.rotation.y += delta * 0.7
+    group3.rotation.y += delta * 0.6
 
     renderer.render(scene, camera)
     requestAnimationFrame(draw)
