f056ecae951646aa38d314a4da2aa99bd8b28000
jjanmo <jjanmo07@hanmail.net>
2022-08-03 00:32:55 +0900

    feat: introduce what is trackballcontrols

--- part9/06_camera-control/src/app.js
+++ part9/06_camera-control/src/app.js
@@ -1,4 +1,5 @@
 import './style.css'
-import example from './exmaples/01_options'
+// import example from './exmaples/01_orbit-controls'
+import example from './exmaples/02_trackball-controls'
 
 example()
