66966b702f802828b189763e58c93d1de1d38f70
jjanmo <jjanmo07@hanmail.net>
2022-08-07 21:52:25 +0900

    feat: what is pointlight

--- part9/08_light/src/app.js
+++ part9/08_light/src/app.js
@@ -1,6 +1,7 @@
 import './style.css'
 // import example from './exmaples/01_basic'
 // import example from './exmaples/02_light-animtaion'
-import example from './exmaples/03_shadow'
+// import example from './exmaples/03_shadow'
+import example from './exmaples/04_point-light'
 
 example()
