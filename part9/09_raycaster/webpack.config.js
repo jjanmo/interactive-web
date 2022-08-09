2608df3a0b6c25dc341873643e1b25e44dda0a78
jjanmo <jjanmo07@hanmail.net>
2022-08-09 23:04:57 +0900

    feat: what is raycaster

---
+++ part9/09_raycaster/webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/app.js',
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].min.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: [
+          process.env.NODE_ENV === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        loader: 'file-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      minify:
+        process.env.NODE_ENV === 'production'
+          ? {
+              collapseWhitespace: true,
+              removeComments: true,
+            }
+          : false,
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({ filename: `style.css` }),
+  ],
+  devServer: {
+    liveReload: true,
+  },
+  optimization: {
+    minimizer:
+      process.env.NODE_ENV === 'production'
+        ? [
+            new TerserPlugin({
+              terserOptions: {
+                compress: {
+                  drop_console: true,
+                },
+              },
+            }),
+          ]
+        : [],
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+}
