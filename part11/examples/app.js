9f6af669c52142f7ec6ea44495ed5569cf0a5adc
jjanmo <jjanmo07@hanmail.net>
2022-12-30 02:44:44 +0900

    feat: how to use request-animation

--- part11/examples/app.js
+++ part11/examples/app.js
@@ -1,9 +1,27 @@
-const $picture = document.querySelector('.picture');
+(function () {
+  const $picture = document.querySelector('.picture');
+  const $rocket = document.querySelector('.rocket');
 
-document.addEventListener('click', (e) => {
-  const { width, height } = $picture.getBoundingClientRect();
-  const posX = e.clientX - width / 2;
-  const posY = e.clientY - height / 2;
+  document.addEventListener('click', (e) => {
+    const { width, height } = $picture.getBoundingClientRect();
+    const posX = e.clientX - width / 2;
+    const posY = e.clientY - height / 2;
 
-  $picture.style.transform = `translate(${posX}px, ${posY}px)`;
-});
+    $picture.style.transform = `translate(${posX}px, ${posY}px)`;
+  });
+
+  let loc = 0;
+  let rafId;
+
+  function launch() {
+    loc = loc + 20;
+    $rocket.style.bottom = `${loc}px`;
+
+    rafId = requestAnimationFrame(launch);
+
+    if (loc > 800) {
+      cancelAnimationFrame(rafId);
+    }
+  }
+  launch();
+})();
