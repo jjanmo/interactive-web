3b3290a53c3647d6a563bac40751c30f1c057359
jjanmo <jjanmo07@hanmail.net>
2023-04-23 22:11:09 +0900

    feat: add mouseover/out feature and fix bug(answers vs answer)

--- part1_interactive-web/ver2.0/mouseover/app.js
+++ part1_interactive-web/ver2.0/mouseover/app.js
@@ -3,8 +3,10 @@ let x = 0,
   targetX = 0,
   targetY = 0;
 
-const SPEED = 0.09;
+const SPEED = 0.07;
 const $cursor = document.querySelector('.cursor');
+const $circle = document.querySelector('.circle');
+const $answers = document.querySelectorAll('.answer');
 
 window.addEventListener('mousemove', (e) => {
   x = e.pageX;
@@ -15,10 +17,17 @@ const animate = () => {
   targetX += (x - targetX) * SPEED;
   targetY += (y - targetY) * SPEED;
 
-  $cursor.style.top = `${targetY}px`;
-  $cursor.style.left = `${targetX}px`;
-
+  $cursor.style.transform = `translate3d(${targetX}px, ${targetY}px, 0)`;
   requestAnimationFrame(animate);
 };
 
 animate();
+
+$answers.forEach(($) => {
+  $.addEventListener('mouseover', () => {
+    $circle.style.transform = `translate(-50%, -50%) scale(0.4)`;
+  });
+  $.addEventListener('mouseout', () => {
+    $circle.style.transform = `translate(-50%, -50%) scale(1)`;
+  });
+});
