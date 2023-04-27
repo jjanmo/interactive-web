20582722ee790c5f4e58b3664fb016bc94de36e4
jjanmo <jjanmo07@hanmail.net>
2023-04-27 23:43:46 +0900

    feat: add soft movement

--- part1_interactive-web/ver2.0/sirup/app.js
+++ part1_interactive-web/ver2.0/sirup/app.js
@@ -1,4 +1,6 @@
-let x = 0;
+let x = 0,
+  targetX = 0;
+const speed = 0.09;
 
 const $panel = document.querySelector('.panel img');
 const $artist = document.querySelector('.artist img');
@@ -7,9 +9,18 @@ const $date = document.querySelector('.date img');
 
 window.addEventListener('mousemove', (e) => {
   x = e.pageX - window.innerWidth / 2;
-
-  $panel.style.transform = `translateX(${x / 50}px) rotateY(${-x / 70}deg)`;
-  $artist.style.transform = `translateX(${-x / 40}px)`;
-  $text.style.transform = `translateX(${-x / 100}px)`;
-  $date.style.transform = `translateX(${x / 35}px)`;
 });
+
+const animate = () => {
+  targetX += (x - targetX) * speed;
+
+  $panel.style.transform = `translateX(${targetX / 50}px) rotateY(${
+    -x / 25
+  }deg)`;
+  $artist.style.transform = `translateX(${-targetX / 40}px)`;
+  $text.style.transform = `translateX(${-targetX / 100}px)`;
+  $date.style.transform = `translateX(${targetX / 35}px)`;
+
+  window.requestAnimationFrame(animate);
+};
+animate();
