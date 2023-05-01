be3d13bf23ca6a5f1c6801df5a59be6959a515ce
jjanmo <jjanmo07@hanmail.net>
2023-05-01 19:28:57 +0900

    feat: add rendering images logic and add depth ui

--- part1_interactive-web/ver2.0/parallax1/app.js
+++ part1_interactive-web/ver2.0/parallax1/app.js
@@ -1,10 +1,44 @@
 const $progress = document.querySelector('.progress');
 const $filled = document.querySelector('.filled');
+const $depth = document.querySelector('.depth');
+const animals = document.querySelectorAll('.animal');
 
-window.addEventListener('scroll', () => {
+const directionMap = {
+  0: 'left',
+  1: 'center',
+  2: 'right',
+};
+
+function renderDepth() {
+  const depth = Math.ceil(window.scrollY / 10);
+  if (depth > 10) {
+    $depth.style.opacity = 1;
+    $depth.querySelector('.value').textContent = `${depth}`;
+  } else $depth.style.opacity = 0;
+}
+
+function handleScroll() {
   const currentRatio =
     window.scrollY /
     (document.documentElement.scrollHeight - window.innerHeight);
 
   $filled.style.width = `${100 * currentRatio}%`;
-});
+
+  renderDepth();
+}
+
+function init() {
+  animals.forEach(($animal) => {
+    const marginTop = Math.floor(Math.random() * 150 + 100);
+    const marginBottom = Math.floor(Math.random() * 150 + 100);
+    const directionKey = Math.floor(Math.random() * 3);
+
+    $animal.style.margin = `${marginTop}px 0 ${marginBottom}px`;
+    $animal.classList.add(directionMap[directionKey]);
+  });
+
+  window.addEventListener('scroll', handleScroll);
+  renderDepth();
+}
+
+init();
