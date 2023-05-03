562e00cbf39c73b3262446d7e2f6ba2506b02e31
jjanmo <jjanmo07@hanmail.net>
2023-05-03 21:27:32 +0900

    fix: fix changing of header ui style

--- part1_interactive-web/ver2.0/parallax2/app.js
+++ part1_interactive-web/ver2.0/parallax2/app.js
@@ -2,17 +2,23 @@ const header = document.querySelector('.header');
 const brunch = document.querySelector('.brunch');
 const filled = document.querySelector('.filled');
 const overlay = document.querySelector('.overlay');
+const logoLink = document.querySelector('.logo-link');
+const icons = document.querySelector('.icons');
 
 const setFixedHeader = () => {
   header.classList.add('fixed-header');
   brunch.classList.add('fixed-text');
   filled.classList.add('fixed-progress');
+  logoLink.classList.add('fixed-logo');
+  icons.classList.add('fixed-icons');
 };
 
 const removeFixedHeader = () => {
   header.classList.remove('fixed-header');
   brunch.classList.remove('fixed-text');
   filled.classList.remove('fixed-progress');
+  logoLink.classList.remove('fixed-logo');
+  icons.classList.remove('fixed-icons');
 };
 
 const handleScroll = () => {
