b0cb83cc248bbd73cabe0a522a7f33f9062c5aa4
jjanmo <jjanmo07@hanmail.net>
2023-05-06 00:45:17 +0900

    feat: update mousemove effect

--- part1_interactive-web/ver2.0/parallax3/app.js
+++ part1_interactive-web/ver2.0/parallax3/app.js
@@ -1,4 +1,13 @@
 const images = document.querySelectorAll('.image');
+const image7 = document.querySelector('.image7');
+const mousemoveImages = [
+  image7,
+  images[5], // image6
+  images[4], // image5
+];
+
+const speed = 0.09;
+let targetX = 0;
 
 window.addEventListener('scroll', () => {
   const scrollY = window.scrollY;
@@ -10,3 +19,23 @@ window.addEventListener('scroll', () => {
     }px)`;
   });
 });
+
+window.addEventListener('mousemove', (e) => {
+  const x = e.pageX; // clientX  로 해도 같음(이 구현은 페이지가 스크롤되지 않는 상태에서 일어나기때문에)
+  targetX += (x - targetX) * speed;
+});
+
+const animate = () => {
+  const distance = targetX - window.innerWidth / 2;
+
+  mousemoveImages.forEach((image, index) => {
+    console.log(distance / ((index + 1) * 50));
+    image.style.transform = `translateX(${-(
+      distance /
+      ((index + 1) * 25)
+    )}px) scale(1.05)`;
+  });
+
+  window.requestAnimationFrame(animate);
+};
+animate();
