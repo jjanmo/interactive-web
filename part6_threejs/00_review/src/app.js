cf33d152ff70de8258d8322e19734c04a5852501
jjanmo <jjanmo07@hanmail.net>
2023-11-20 23:24:49 +0900

    feat: 15_cube 프로젝트 생성 및 설정

--- part6_threejs/00_review/src/app.js
+++
@@ -1,6 +0,0 @@
-// import { App } from './examples/01_basic'
-import { App } from './examples/02_geometry'
-
-window.onload = function () {
-  new App()
-}
---
+++ part6_threejs/15_cube/src/main.js
@@ -0,0 +1,10 @@
+console.log('hell');
+import * as THREE from 'three';
+
+function init() {
+  console.log(THREE);
+}
+
+window.addEventListener('load', () => {
+  init();
+});
