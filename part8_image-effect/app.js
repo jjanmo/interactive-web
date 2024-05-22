2e0813385455b3687c1df8b3f1f0a58b9511e6c4
jjanmo <jjanmo07@hanmail.net>
2024-05-22 23:08:18 +0900

    refactor: 기능단위로 함수 분리

--- part8_image-effect/app.js
+++ part8_image-effect/app.js
@@ -1,16 +1,34 @@
-const witdh = window.innerWidth;
-const height = window.innerHeight;
-const COL = 20;
-const ROW = 10;
+const COL = 8;
+const ROW = 4;
 
-const container = document.querySelector('.container');
+const updateContainer = (elements) => {
+  const container = document.querySelector('.container');
+  container.innerHTML = '';
+  container.appendChild(elements);
+};
 
-for (let i = 0; i < COL; i++) {
-  for (let j = 0; j < ROW; j++) {
-    const div = document.createElement('div');
-    div.style.width = `${witdh / COL}px`;
-    div.style.height = `${height / ROW}px`;
-    div.classList.add('.image-box');
-    container.appendChild(div);
+const makeImageBoxes = () => {
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < COL; i++) {
+    for (let j = 0; j < ROW; j++) {
+      const div = document.createElement('div');
+      div.style.width = `${100 / COL}%`;
+      div.style.height = `${100 / ROW}%`;
+      div.classList.add('.image-box');
+      fragment.appendChild(div);
+    }
   }
-}
+  return fragment;
+};
+
+const generateLayout = () => {
+  const imageBoxes = makeImageBoxes();
+  updateContainer(imageBoxes);
+};
+
+const init = () => {
+  window.addEventListener('resize', generateLayout);
+  generateLayout();
+};
+
+init();
