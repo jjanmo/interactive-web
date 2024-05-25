2e685fb85f96254c4a9544620cd0c58abe54ecab
jjanmo <jjanmo07@hanmail.net>
2024-05-25 12:47:13 +0900

    feat: 이미지 포지션을 통한 하나의 이미지로 보이도록 수정

--- part8_image-effect/app.js
+++ part8_image-effect/app.js
@@ -1,5 +1,7 @@
 const COL = 8;
 const ROW = 4;
+const IMAGE_POSITION_ELEM_WIDTH = COL * 100;
+const IMAGE_POSITION_ELEM_HEIGHT = ROW * 100;
 
 const updateContainer = (elements) => {
   const container = document.querySelector('.image-container');
@@ -9,20 +11,28 @@ const updateContainer = (elements) => {
 
 const makeImageBoxes = () => {
   const fragment = document.createDocumentFragment();
-  for (let i = 0; i < COL; i++) {
-    for (let j = 0; j < ROW; j++) {
-      const delayTime = (COL - i - j * 0.5) * 0.25; // 아래 → 위
+  for (let i = 0; i < ROW; i++) {
+    for (let j = 0; j < COL; j++) {
+      const delayTime = (COL - i - j * 0.5) * 0.25;
 
-      const div = document.createElement('div');
-      div.style.width = `${100 / COL}%`;
-      div.style.height = `${100 / ROW}%`;
-      div.style.transitionDelay = `${delayTime}s`;
-      div.classList.add('image-box');
+      const imageBox = document.createElement('div');
+      imageBox.style.width = `${100 / COL}%`;
+      imageBox.style.height = `${100 / ROW}%`;
+      imageBox.style.transitionDelay = `${delayTime}s`;
+      imageBox.classList.add('image-box');
 
-      fragment.appendChild(div);
+      const imagePosition = document.createElement('div');
+      imagePosition.style.width = `${IMAGE_POSITION_ELEM_WIDTH}%`;
+      imagePosition.style.height = `${IMAGE_POSITION_ELEM_HEIGHT}%`;
+      imagePosition.style.top = `-${i * 100}%`;
+      imagePosition.style.left = `-${j * 100}%`;
+      imagePosition.classList.add('image-position');
+
+      imageBox.appendChild(imagePosition);
+      fragment.appendChild(imageBox);
 
       setTimeout(() => {
-        div.classList.add('active');
+        imageBox.classList.add('active');
       }, 200);
     }
   }
