8870021187776ffcb7e9aa30a97a8850da378094
jjanmo <jjanmo07@hanmail.net>
2025-11-30 01:07:39 +0900

    fix: 이해를 돕는 주석 추가 + 변수 변경

--- part10_scroll/section2-1/app.js
+++ part10_scroll/section2-1/app.js
@@ -3,10 +3,14 @@ function init() {
   const $percent = document.querySelector('.percent');
 
   window.addEventListener('scroll', () => {
+    /** 현재 스크롤된 위치(스크롤 위치를 나타내는 값) */
     const scrollTop = document.documentElement.scrollTop;
-    const scrollHeight = document.documentElement.scrollHeight;
+    /** 전체 문서의 높이(보이지 않는 영역까지 포함한 전체 콘텐츠의 높이) */
+    const documentHeight = document.documentElement.scrollHeight;
+    /** 브라우저 창의 높이(화면에 보이는 높이) */
     const clientHeight = document.documentElement.clientHeight;
-    const scrollableHeight = scrollHeight - clientHeight;
+    /** 스크롤 가능한 최대 높이 */
+    const scrollableHeight = documentHeight - clientHeight;
     const ratio = scrollTop / scrollableHeight;
 
     $bar.style.width = `${ratio * 100}%`;
